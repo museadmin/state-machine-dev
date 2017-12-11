6b7d24c64f1fb7a12ce3ffdadf4cf0c32877a3c7
Bradley Atkins <bradley.atkins@bjss.com>
2017-12-11 10:46:42 +0000

    Initial commit

---
+++ actions/echo_flag_to_stdout.rb
@@ -0,0 +1,20 @@
+class EchoFlagToSTDOUT
+
+  attr_accessor :flag, :phase, :state, :payload
+
+  def initialize
+    @flag = 'ECHO_FLAG_TO_STDOUT'
+    @phase = 'RUNTIME'
+    @state = 'ACT'
+    @payload = nil
+  end
+
+  def execute(args)
+
+    if args[:phase] == phase && @state == 'ACT'
+      puts @flag
+    end
+
+  end
+
+end
