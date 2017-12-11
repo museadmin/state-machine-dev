36c7e997ba7ef7de44840c04541cf18a5b0a6949
Bradley Atkins <bradley.atkins@bjss.com>
2017-12-11 17:20:20 +0000

    Added example action class

--- actions/echo_flag_to_stdout.rb
+++ actions/echo_flag_to_stdout.rb
@@ -1,17 +1,21 @@
+require 'state/actions/action'
+require 'state/support/data_access_sqlite3'
+
 class EchoFlagToStdout
 
   attr_accessor :flag, :phase, :state, :payload
 
-  def initialize
+  def initialize(control)
     @flag = 'ECHO_FLAG_TO_STDOUT'
     @phase = 'RUNTIME'
     @state = 'ACT'
-    @payload = nil
+    @payload = 'NULL'
+    save_action(self, control)
   end
 
   def execute(args)
 
-    if args[:phase] == phase && @state == 'ACT'
+    if args[:phase] == @phase && @state == 'ACT'
       puts @flag
     end
 
